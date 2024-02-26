70ad4cb8acf0630fa4d2864987e493270818dcca
Soroush Saberi <soroush.13830o@gmail.com>
2024-02-26 18:09:32 +0330

    test2

--- Algorithms/src/TwoSum.java
+++ Algorithms/src/TwoSum.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class TwoSum {
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        // Create a map to store the indices of elements
+        Map<Integer, Integer> numIndices = new HashMap<>();
+
+        // Iterate over the array
+        for (int i = 0; i < nums.length; i++) {
+            // Calculate the complement (target - num)
+            int complement = target - nums[i];
+
+            // Check if the complement is already in the map
+            if (numIndices.containsKey(complement)) {
+                // Return the indices of the two numbers
+                return new int[]{numIndices.get(complement), i};
+            }
+
+            // Otherwise, add the current number and its index to the map
+            numIndices.put(nums[i], i);
+        }
+
+        // If no solution is found, return an empty array
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        String s = "cum";
+        System.out.println(s.substring(1,2));
+    }
 }
--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -13,9 +13,19 @@ public class Exercises2 {
     You can return the answer in any order.
     */
 
-    public int[] twoSum(int[] nums, int target) {
-        // TODO
-        return null;
+    public static int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        int[] result = new int[2];
+        for(int i = 0 ; i < length - 1 ;i++){
+            for(int j = i + 1 ; j < length; j++){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     /*
@@ -48,22 +58,93 @@ public class Exercises2 {
     Given a roman numeral, convert it to an integer.
     */
 
-    public int romanToInt(String s) {
-        // TODO
-        return 0;
+    public static int romanToInt(String s) {
+        int sum = 0;
+        char[] charArray = s.toCharArray();
+        int length = s.length();
+        for(int i = 0 ; i <length ; i++){
+            switch (charArray[i]) {
+                case 'I':
+                    if (i < length - 1 && charArray[i + 1] == 'V'){
+                        sum += 4;
+                        i++;}
+                    else if (i < length - 1 &&charArray[i + 1] == 'X'){
+                        sum += 9;
+                        i++;}
+                    else
+                        sum += 1;
+                    break;
+                case 'V':
+                    sum += 5;
+                    break;
+                case 'X':
+                    if (i < length - 1 &&charArray[i + 1] == 'L'){
+                        sum += 40;
+                        i++;}
+                    else if (i < length - 1 && charArray[i + 1] == 'C'){
+                        sum += 90;
+                        i++;}
+                    else
+                        sum += 10;
+                    break;
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'C':
+                    if (i < length - 1 &&charArray[i + 1] == 'D'){
+                        sum += 400;
+                        i++;}
+                    else if (i < length - 1 && charArray[i + 1] == 'M'){
+                        sum += 900;
+                        i++;}
+                    else
+                        sum += 1000;
+                    break;
+                case 'D':
+                    sum += 500;
+                    break;
+                case 'M':
+                    sum += 1000;
+                    break;
+            }
+            }
+        return sum;
     }
+
 
     /*
     Given an array nums of distinct integers, return all the possible permutations.
     You can return the answer in any order.
     */
 
-    public List<List<Integer>> permute(int[] nums) {
-        // TODO
-        return null;
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        possibleWays(nums, new ArrayList<>(), result);
+        return result;
     }
 
+    private static void possibleWays(int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (current.contains(nums[i])) {
+                continue;
+            }
+            current.add(nums[i]);
+            possibleWays(nums, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+
+
     public static void main(String[] args) {
-        // test your code here!
+        String s = "MCMXCIV";
+        int[] nums = {2 , 7 , 8 , 10};
+        System.out.println(romanToInt(s));
+        System.out.println(twoSum(nums , 9)[0] + " " + twoSum(nums , 9)[1]);
     }
 }
